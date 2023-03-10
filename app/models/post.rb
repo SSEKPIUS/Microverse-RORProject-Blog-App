335020b8788668f96cbd3db9bbc0c6ec78104d7c
Pius <ssekweyamapius@gmail.com>
2023-03-10 21:25:40 +0300

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,15 +3,15 @@ class Post < ApplicationRecord
     has_many :comments
     has_many :likes
   
-    after_save :update_posts_counter
+    after_save :update_post_counter
   
     def recent_comments
       comments.order(created_at: :desc).limit(5)
     end
   
     private
-    
+
     def update_post_counter
-      author.increment!(:posts_counter)
+      author.increment!(:post_counter)
     end
 end
