e206a922366d46214f7f14cdbbf0491ac245a26d
Pius <ssekweyamapius@gmail.com>
2023-03-17 21:07:09 +0300

    linters

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,14 +16,18 @@ RSpec.describe User, type: :model do
     let(:user) { User.create!(name: 'Alice', post_counter: 0) }
 
     it 'returns all posts if the specified limit is greater than or equal to the total number of posts' do
-      post1 = user.posts.create!(title: 'Post 1', text: 'Body 1', comments_counter: 0, likes_counter: 0, created_at: 1.day.ago)
-      post2 = user.posts.create!(title: 'Post 2', text: 'Body 2', comments_counter: 0, likes_counter: 0, created_at: 2.days.ago)
-      post3 = user.posts.create!(title: 'Post 3', text: 'Body 3', comments_counter: 0, likes_counter: 0, created_at: 3.days.ago)
+      post1 = user.posts.create!(title: 'Post 1', text: 'Body 1', comments_counter: 0, likes_counter: 0,
+                                 created_at: 1.day.ago)
+      post2 = user.posts.create!(title: 'Post 2', text: 'Body 2', comments_counter: 0, likes_counter: 0,
+                                 created_at: 2.days.ago)
+      post3 = user.posts.create!(title: 'Post 3', text: 'Body 3', comments_counter: 0, likes_counter: 0,
+                                 created_at: 3.days.ago)
       expect(user.recent_posts).to eq([post1, post2, post3])
     end
 
     it 'returns the most recent posts up to the specified limit' do
-      post1 = user.posts.create!(title: 'Post 1', text: 'Body 1', comments_counter: 0, likes_counter: 0, created_at: 1.day.ago)
+      post1 = user.posts.create!(title: 'Post 1', text: 'Body 1', comments_counter: 0, likes_counter: 0,
+                                 created_at: 1.day.ago)
       expect(user.recent_posts).to eq([post1])
     end
   end
