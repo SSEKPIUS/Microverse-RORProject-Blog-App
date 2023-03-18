1339ff518604f4fa0d5c33da556781f077c359d4
Pius <ssekweyamapius@gmail.com>
2023-03-18 10:27:12 +0300

    post and application  controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,39 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
+    @posts = Post.find_by(author_id: params[:id])
   rescue StandardError => e
     redirect_to users_path(@user, param: e)
   end
 
   def show
     @post = Post.find_by(id: params[:id])
-    # begin
-    #   @post = Post.find_by(id: params[:id])
-    # rescue => error
-    #   redirect_to users_path(@user, param: 'No such Post ID')
-    # end
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+    @post = Post.new
+    render :new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+    author = current_user
+    @post.author = author
+    if @post.save
+      flash[:success] = 'Post successfully added!'
+      redirect_to user_posts_path(current_user, @post)
+    else
+      flash.now[:error] = 'Post creation failed!'
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
