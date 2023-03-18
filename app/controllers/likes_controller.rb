cdb24fd8e8e5084b3f72663a102ad951c004a62b
Pius <ssekweyamapius@gmail.com>
2023-03-18 10:40:19 +0300

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,12 @@
 class LikesController < ApplicationController
-    def create
-        post = Post.find(params[:post_id])
-        author = current_user
-        liked = Like.find_by(author:, post:)
+  def create
+    post = Post.find(params[:post_id])
+    author = current_user
+    liked = Like.find_by(author: author, post: post)
 
-        return if liked.present?
-        
-        like = post.likes.create(author:)
-        redirect_to user_post_path(post.author, post) if like.persisted?
-      end
+    return if liked.present?
+
+    like = post.likes.create(author: author)
+    redirect_to user_post_path(post.author, post) if like.persisted?
+  end
 end
