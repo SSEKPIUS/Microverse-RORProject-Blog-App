a51c088cc2da67df7f63abbe9442d54d0975785d
Pius <ssekweyamapius@gmail.com>
2023-03-18 10:34:37 +0300

    linters

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,7 +3,9 @@ class LikesController < ApplicationController
         post = Post.find(params[:post_id])
         author = current_user
         liked = Like.find_by(author:, post:)
+
         return if liked.present?
+        
         like = post.likes.create(author:)
         redirect_to user_post_path(post.author, post) if like.persisted?
       end
--- spec/requests/likes_request_spec.rb
+++ spec/requests/likes_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "Likes", type: :request do
-    it 'renders data' do
-        expect(true).to be(true)
-    end
+RSpec.describe 'Likes', type: :request do
+  it 'renders data' do
+    expect(true).to be(true)
+  end
 end
